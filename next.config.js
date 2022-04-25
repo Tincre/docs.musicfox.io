54416778016df380b201b758db94d006a0e7d69f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-04-25 12:04:21 -0500

    :sparkles: add all slug redirects per https://nextjs.org/docs/api-reference/next.config.js/redirects #1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://community.b00st.com',
+        permanent: true,
+      },
+      {
+        source: '/:slug*',
+        destination: 'https://community.b00st.com',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
